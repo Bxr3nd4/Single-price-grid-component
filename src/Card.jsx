cd4b54101d7e65b0741b5dfd753ac80b7aafb1a3
Bxr3nd4 <Fiona5109@hotmail.com>
2024-01-03 18:31:45 -0500

    Adding styles to the card.

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,41 +4,39 @@ function Card() {
   return (
     <div className="full-card">
       <div className="first-section">
-        <h1>Join our community</h1>
-        <p>30-day, hassle-free money back guarantee</p>
-        <p>
-          Gain access to our full library of tutorials along with expert code
-          reviews.
-          <br />
-          Perfect for any developers who are serious about honing their skills.
-        </p>
+        <h2>Join our community</h2>
+        <div class="subtitle-1">30-day, hassle-free money back guarantee</div>
+        <div className="subtitle">
+          <p>
+            Gain access to our full library of tutorials along with expert code
+            reviews.
+          </p>
+          <p>
+            Perfect for any developers who are serious about honing their
+            skills.
+          </p>
+        </div>
       </div>
       <div className="second-section">
         <div className="card-1">
-          <h1>Monthly Subscription</h1>
-          <p>
-            <strong>$29</strong>per month
-          </p>
-          <p>Full access for less than $1 a day</p>
-          <button>Sign Up</button>
+          <h3>Monthly Subscription</h3>
+          <div class="subscription-price">
+            $29 <span>per month</span>
+          </div>
+          <p className="">Full access for less than $1 a day</p>
+          <a href="#">Sign Up</a>
         </div>
-        <div className="card-2">
-          <h1>Why Us</h1>
-          <p>
-            Tutorials by industry experts
-            <br />
-            Peer & expert code review
-            <br />
-            Coding excercises
-            <br />
-            Access to our GitHub repos
-            <br />
-            Community forum
-            <br />
-            Flashcard decks
-            <br />
-            New videos every week
-          </p>
+        <div class="card-2">
+          <h3>Why Us</h3>
+          <ul>
+            <li>Tutorials by industry experts</li>
+            <li>Peer &amp; expert code review</li>
+            <li>Coding exercises</li>
+            <li>Access to our GitHub repos</li>
+            <li>Community forum</li>
+            <li>Flashcard decks</li>
+            <li>New videos every week</li>
+          </ul>
         </div>
       </div>
     </div>
